acf5a25ced1c98ce0fe505d966efcb935e6432b7
alexisboyd1 <alexisboyd1@berkeley.edu>
2015-02-26 20:00:59 -0800

    Switched sign

--- PolarPlot.py
+++ PolarPlot.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 
 theta = np.arange(0, 2*np.pi,.01)
-r = 1 + np.cos(theta)
+r = 1 -np.cos(theta)
 ax = plt.subplot(111, polar=True)
 ax.plot(theta, r, color='r', linewidth=3)
 ax.set_rmax(2.0)
